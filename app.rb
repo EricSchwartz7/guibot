70e33430432cdad821badffbb3075d3a711cbce8
github username <github email address>
2016-12-23 11:21:54 -0500

    removing trigger word

--- app.rb
+++ app.rb
@@ -7,9 +7,15 @@ get '/' do
   erb :show
 end
 
+def get_resp(repo_url)
+  resp = HTTParty.get(repo_url)
+  JSON.parse resp.body
+end
+
 ## Receive post at '/gateway' and send to repo_url
 post '/gateway' do
-  message = params[:text].gsub(params[:trigger_word], '').strip
+  message = params[:text]
+  # .gsub(params[:trigger_word], '').strip
 
   action, repo = message.split('_').map {|c| c.strip.downcase }
   repo_url = "https://api.github.com/repos/#{repo}"
@@ -17,20 +23,14 @@ post '/gateway' do
 
   case action
     when 'issues'
-      resp = HTTParty.get(repo_url)
-      resp = JSON.parse resp.body
+      resp = get_resp(repo_url)
       respond_message "There are #{resp['open_issues_count']} open issues on #{repo}."
     when 'forks'
-      resp = HTTParty.get(repo_url)
-      resp = JSON.parse resp.body
+      resp = get_resp(repo_url)
       respond_message "There are #{resp['forks']} forks on #{repo}."
     when 'fire'
       fire_text = []
       100.times { fire_text << ":fire:" }
-      respond_message "#{fire_text.join}"
-    when 'FIRE'
-      fire_text = []
-      10000.times { fire_text << ":fire:" }
       respond_message "#{fire_text.join}"
     else
       respond_message "Say 'issues' or 'forks', ya moron!"
